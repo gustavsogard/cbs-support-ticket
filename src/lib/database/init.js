682ce618eb63277e64d6b611f1052da09fdb6351
thel22ab <thel22ab@student.cbs.dk>
2024-05-08 13:23:58 +0200

    Update database

--- src/lib/database/init.js
+++ src/lib/database/init.js
@@ -7,25 +7,29 @@ const init = () => {
       "CREATE TABLE IF NOT EXISTS support_tickets (ticketId INTEGER PRIMARY KEY, status TEXT DEFAULT 'created', title TEXT, description TEXT, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, createdBy TEXT, assignedTo TEXT, affectedUser TEXT, category TEXT)"
     );
     db.run(
-      "INSERT INTO support_tickets (title, description, createdBy, assignedTo, affectedUser, category) VALUES ('Issue with printer', 'The color printer on the 2nd floor is not working.', 'alice@example.com', 'bob@example.com', 'charlie@example.com', 'hardware')"
+      `INSERT INTO support_tickets (title, description, createdBy, assignedTo, affectedUser, category) VALUES 
+      ('Outlook er stoppet med at åbne automatisk', 'Den plejede altid at åbne automatisk, nu gør den ikke længere.', 'A1001', 'E1001', 'A1001', 'Hardware'),
+      ('Studentermedarbejders adgang til system', 'De har brug for adgang til systemet for at kunne udføre deres arbejde.', 'A1001', 'E1001', 'A1001', 'Acess management'),
+      ('Min Mycbs ser mærkelig', 'Der mangler en masse ikoner.', 'S162392', 'E1001', 'S162392', 'Software'),
+      `
     );
     db.run(
       "CREATE TABLE IF NOT EXISTS it_employees (userId INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT, status TEXT, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, passwordChangedAt TIMESTAMP, employeeNumber TEXT)"
     );
     db.run(
-      "INSERT INTO it_employees (name, email, password, status, employeeNumber) VALUES ('Bob Smith', 'bob@example.com', 'password123', 'active', 'E1001')"
+      "INSERT INTO it_employees (name, email, password, status, employeeNumber) VALUES ('Bob Smith', 'bs.it@cbs.dk', 'password123', 'active', 'E1001')"
     );
     db.run(
       "CREATE TABLE IF NOT EXISTS students (userId INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT, status TEXT, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, passwordChangedAt TIMESTAMP, studentNumber TEXT)"
     );
     db.run(
-      "INSERT INTO students (name, email, password, status, studentNumber) VALUES ('Alice Johnson', 'alice@student.edu', 'mypassword', 'enrolled', 'S1001')"
+      "INSERT INTO students (name, email, password, status, studentNumber) VALUES ('Alice Johnson', 'aljo23ab@student.cbs.dk', 'mypassword', 'enrolled', 'S162392')"
     );
     db.run(
       "CREATE TABLE IF NOT EXISTS academics (userId INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT, status TEXT, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, passwordChangedAt TIMESTAMP, academicId TEXT, title TEXT, department TEXT, phone STRING)"
     );
     db.run(
-      "INSERT INTO academics (name, email, password, title, department, phone, academicId) VALUES ('Dr. Charlie Brown', 'charlie@academia.edu', 'securepass', 'Professor', 'Computer Science', '123-456-7890', 'A1001')"
+      "INSERT INTO academics (name, email, password, title, department, phone, academicId) VALUES ('Dr. Charlie Brown', 'cb.fi@cbs.dk', 'securepass', 'Professor', 'Finansiering', '123-456-7890', 'A1001')"
     );
   });
 };
